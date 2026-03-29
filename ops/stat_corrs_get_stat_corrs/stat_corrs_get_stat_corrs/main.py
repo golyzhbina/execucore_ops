9dbb57828ec6fdcefbe2c32d0f1b4cb9d27b3f10
golyzhbina <golubrysa@gmail.com>
2026-03-29 14:28:05 +0700

    update stat_corrs_get_stat_corrs

--- ops/stat_corrs_get_stat_corrs/stat_corrs_get_stat_corrs/main.py
+++ ops/stat_corrs_get_stat_corrs/stat_corrs_get_stat_corrs/main.py
@@ -34,9 +34,27 @@ from msm_lib import load_obj, dump_obj
 from msm_lib.stat_cors import StatCorsProcessor
 
 traces = load_obj(load_var_and_data(get_var_path("get_stat_corrs__traces_path")))
-window_size = load_var_and_data(get_var_path("get_stat_corrs__window_size"))
+region = load_obj(load_var_and_data(get_var_path("get_stat_corrs__region_path")))
+travel_time = load_obj(load_var_and_data(get_var_path("get_stat_corrs__travel_time_path")))
 
-traces = StatCorsProcessor.get_stats(traces, window_size)
+window_size = load_var_and_data(get_var_path("get_stat_corrs__window_size"))
+amount_iter = load_var_and_data(get_var_path("get_stat_corrs__amount_iter"))
+batch_size = load_var_and_data(get_var_path("get_stat_corrs__batch_size"))
+time_interval = load_var_and_data(get_var_path("get_stat_corrs__time_interval"))
+subtrahend = load_var_and_data(get_var_path("get_stat_corrs__subtrahend"))
+source_coords = load_var_and_data(get_var_path("get_stat_corrs__source_coords"))
+
+traces = StatCorsProcessor.get_stats(
+    traces, 
+    region, 
+    travel_time,
+    source_coords,
+    time_interval, 
+    amount_iter, 
+    batch_size, 
+    window_size, 
+    subtrahend
+)
 
 out_obj_path = get_obj_path("get_stat_corrs__out")
 out_var_path = get_var_path("get_stat_corrs__out")
